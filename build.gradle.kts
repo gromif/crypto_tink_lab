ea3dc8223226a41b502a6ae312db0a190f9f8cda
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-19 01:16:42 +0200

    Change JVM-Target to project var

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,15 +24,12 @@ android {
             isMinifyEnabled = false
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
+
+kotlin {
+    jvmToolchain(project.property("kotlinJvmToolchainVersion").toString().toInt())
+}
 dependencies {
     api(project(":crypto:tink"))
     api(project(":ui:compose-core"))
