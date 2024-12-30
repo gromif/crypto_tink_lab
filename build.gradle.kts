d891fa7a8baece8fe744ecc10dad974d9c238e05
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-30 11:22:26 +0200

    Rename di.core module to di.utils

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation(projects.core.resources)
     implementation(projects.core.utils)
     implementation(projects.core.tink)
-    implementation(projects.di.core)
+    implementation(projects.di.utils)
 
     implementation(libs.androidx.documentfile)
 
